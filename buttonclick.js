33c9161ce3f5ffcc12fab93d6103a6a892200639
Tori <torigcook@gmail.com>
2025-01-19 19:27:36 -0800

    store last fed locally

--- buttonclick.js
+++ buttonclick.js
@@ -1,14 +1,30 @@
+// Check the local storage when the page is loaded
+document.addEventListener("DOMContentLoaded", () => {
+    lastFedTime = localStorage.getItem("storedLastFed");
+    isAm = localStorage.getItem("storedIsAm");
+
+    console.log("Reading local storage as " + isAm);
+
+    if (lastFedTime != null && isAm != null)
+    {
+        updateText(lastFedTime, isAm);
+    }
+
+});
+
 // Add the click event listener to the button
 document.getElementById("feed-button").addEventListener('click', onFeedButtonClicked);
 
+// Grab the text element
 var lastFedElement = document.getElementById("last-fed");
 
-function onFeedButtonClicked() {
-    var d = new Date();
-    var dateString = d.toLocaleTimeString();
-    lastFedElement.innerText = "🐾 The dogs were last fed at " + dateString + " 🐾";
-    
-    if (d.getHours() < 12)
+var lastFedTime;
+
+function updateText(lastFedTimeString, isAm)
+{
+    lastFedElement.innerText = "🐾 The dogs were last fed at " + lastFedTimeString + " 🐾";
+
+    if (isAm == true)
     {
         lastFedElement.classList.remove("default");
         lastFedElement.classList.remove("night-text");
@@ -19,4 +35,16 @@ function onFeedButtonClicked() {
         lastFedElement.classList.remove("morning-text");
         lastFedElement.classList.add("night-text");
     }
+}
+
+function onFeedButtonClicked() {
+    lastFedTime = new Date();
+    var dateString = lastFedTime.toLocaleTimeString();
+    var isAm = lastFedTime.getHours() < 12;
+
+    localStorage.setItem("storedLastFed", dateString);
+    localStorage.setItem("storedIsAm", isAm);
+    console.log("Setting local storage to " + isAm);
+
+    updateText(dateString, isAm);
 }
